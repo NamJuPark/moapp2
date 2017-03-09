1b156d7504d12d453f4622f69fa65f6990115707
NamJuPark <qkrska66@hanyang.ac.kr>
2017-03-09 15:00:52 +0900

    moapp

---
+++ app/src/main/java/com/example/myapplication2/Main2Activity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+    Button b1;
+    EditText e1,e2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        e1 = (EditText)findViewById(R.id.editText);
+        e2 = (EditText)findViewById(R.id.editText2);
+
+        b1 = (Button)findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String price = e1.getText().toString();
+                String num = e2.getText().toString();
+
+                int result = Integer.parseInt(price) * Integer.parseInt(num);
+
+                Toast.makeText(getApplicationContext(),"Total price of apple : "+result, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+
+
+
+
+    }
+}
