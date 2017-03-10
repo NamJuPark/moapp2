29c836cf52e14c46fd602e1fa619f7924fbd4c0a
NamJuPark <qkrska66@hanyang.ac.kr>
2017-03-10 11:54:03 +0900

    moapp

--- app/src/main/java/com/example/myapplication2/Main5Activity.java
+++ app/src/main/java/com/example/myapplication2/Main5Activity.java
@@ -2,13 +2,67 @@ package com.example.myapplication2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main5Activity extends AppCompatActivity {
+    Button b1;
+    EditText e1,e2,e3;
+    CheckBox c1;
+    TextView price,num;
+    int total_price, total_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        b1 = (Button)findViewById(R.id.button4);
+        e1 = (EditText)findViewById(R.id.editText7);
+        e2 = (EditText)findViewById(R.id.editText8);
+        e3 = (EditText)findViewById(R.id.editText9);
+        c1 = (CheckBox)findViewById(R.id.checkBox);
+        price = (TextView)findViewById(R.id.textView13);
+        num = (TextView)findViewById(R.id.textView11);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                init();
+            }
+        });
+
+    }
+    public void init(){
+        int pizza, spa, salad;
+
+        String pizza_ = e1.getText().toString();
+        if(e1 == null)
+            pizza = 0;
+        else pizza = Integer.parseInt(pizza_);
+
+        String spa_ = e2.getText().toString();
+        if(e2 == null)
+            spa = 0;
+        else spa = Integer.parseInt(spa_);
+
+        String salad_ = e3.getText().toString();
+        if(e3 == null)
+            salad = 0;
+        else salad = Integer.parseInt(salad_);
+
+        total_num = pizza + spa+ salad;
+        total_price = (15000*pizza)+(13000*spa)+(9000 * salad);
+
+        if(c1.isChecked()){
+            total_price = (total_price *90 ) / 100;
+        }
+
+        num.setText(total_num+"개");
+        price.setText(total_price+"원");
 
 
     }
