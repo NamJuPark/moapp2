6d32d1c9e76a83ae5f2ca9d3c6645cfff7f7674d
NamJuPark <qkrska66@hanyang.ac.kr>
2017-03-10 11:15:41 +0900

    moapp

--- app/src/main/java/com/example/myapplication2/Main5Activity.java
+++ app/src/main/java/com/example/myapplication2/Main5Activity.java
@@ -9,5 +9,7 @@ public class Main5Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+
     }
 }
